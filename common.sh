41133abc03942f034295b10068e9c947df884529
Pierre Gergondet <pierre.gergondet@gmail.com>
2015-11-30 09:55:50 +0900

    Re-write all scripts in a functions/main way

--- common.sh
+++ common.sh
@@ -2,112 +2,14 @@
 # This should be sourced, not called.
 set -e
 
-# Directories.
-root_dir=`pwd`
+########################################
+#        -- VERBOSE HANDLING --        #
+########################################
 
-# Check which CI tool we are using
-if `test x${CI_TOOL} = x`; then
-  export CI_TOOL=travis
-fi
-
-# Since our gitlab-ci builder relies on docker we do not need sudo
-if `test x${CI_TOOL} = xgitlab-ci`; then
-  export SUDO_CMD=''
-else
-  if `test x${CI_REQUIRE_SUDO} != x`; then
-    if `test x${CI_REQUIRE_SUDO} = xtrue`; then
-      export SUDO_CMD='sudo'
-    else
-      export SUDO_CMD=''
-    fi
-  else
-    export SUDO_CMD='sudo'
-  fi
-fi
-
-# Check whether this is a PR build or not
-# TODO Get this under gitlab-ci?
-if `test x${TRAVIS_PULL_REQUEST} != x`; then
-  export CI_PULL_REQUEST=${TRAVIS_PULL_REQUEST}
-else
-  export CI_PULL_REQUEST=false
-fi
-
-# Set the repo slug
-if `test x${CI_TOOL} = gitlab-ci`; then
-  export CI_REPO_SLUG=`echo ${CI_PROJECT_DIR}|sed -e's@/builds/@@'`
-else
-  export CI_REPO_SLUG=${TRAVIS_REPO_SLUG}
-fi
-
-# Get the branch
-if `test x${CI_TOOL} = gitlab-ci`; then
-  export CI_BRANCH=${CI_BUILD_REF_NAME}
-else
-  export CI_BRANCH=${TRAVIS_BRANCH}
-fi
-
-build_dir="/tmp/_ci/build"
-install_dir="/tmp/_ci/install"
-
-echo "root_dir: " $root_dir
-echo "build_dir: " $build_dir
-echo "install_dir: " $install_dir
-
-# Check for CI_OS_NAME
-if `test x${CI_OS_NAME} = x`; then
-  if `test x${TRAVIS_OS_NAME} != x`; then
-    export CI_OS_NAME=${TRAVIS_OS_NAME}
-  else
-    export CI_OS_NAME=linux
-  fi
-fi
-
-# Shortcuts.
-git_clone="git clone --quiet --recursive"
-
-# Setup environment variables.
-if [ -d /opt/ros ]; then
-  . /opt/ros/${ROS_DISTRO}/setup.sh
-fi
-
-export LD_LIBRARY_PATH="$install_dir/lib:$LD_LIBRARY_PATH"
-export LTDL_LIBRARY_PATH="$install_dir/lib:$LTDL_LIBRARY_PATH"
-export PKG_CONFIG_PATH="$install_dir/lib/pkgconfig:$PKG_CONFIG_PATH"
-
-if [[ ${CI_OS_NAME} = linux ]]; then
-    export LD_LIBRARY_PATH="$install_dir/lib/`dpkg-architecture -qDEB_BUILD_MULTIARCH`:$LD_LIBRARY_PATH"
-    export LTDL_LIBRARY_PATH="$install_dir/lib/`dpkg-architecture -qDEB_BUILD_MULTIARCH`:$LTDL_LIBRARY_PATH"
-    export PKG_CONFIG_PATH="$install_dir/lib/`dpkg-architecture -qDEB_BUILD_MULTIARCH`/pkgconfig:$PKG_CONFIG_PATH"
-fi
-
-if type "python" > /dev/null; then
-    pythonsite_dir=`python -c "import sys, os; print(os.sep.join(['lib', 'python' + sys.version[:3], 'site-packages']))"`
-    export PYTHONPATH="$install_dir/$pythonsite_dir:$PYTHONPATH"
-fi
-
-if [[ ${CI_OS_NAME} = osx ]]; then
-    # Since default gcc on osx is just a front-end for LLVM...
-    if [[ ${CC} = gcc ]]; then
-      export CXX=g++-4.8
-      export CC=gcc-4.8
-    fi
-fi
-
-# Make cmake verbose.
-export CMAKE_VERBOSE_MAKEFILE=1
-export CTEST_OUTPUT_ON_FAILURE=1
-
-# Create layout.
-mkdir -p "$build_dir"
-mkdir -p "$install_dir"
-
-# Add verbose handling
 # More verbose handling for 'set -e'.
 #
 # Show a traceback if we're using bash, otherwise just a message.
 # Downloaded from: https://gist.github.com/kergoth/3885825
-
 on_exit () {
     ret=$?
     case $ret in
@@ -143,3 +45,147 @@ case "$BASH_VERSION" in
         trap on_error ERR
         ;;
 esac
+
+
+########################################
+#    -- ENVIRONMENT MANIPULATION --    #
+########################################
+
+_gitlab_setup_ci_vars()
+{
+  export CI_REQUIRE_SUDO=false
+  export CI_PULL_REQUEST=false #FIXME Can it be provided by gitlab?
+  export CI_REPO_SLUG=`echo ${CI_PROJECT_DIR}|sed -e's@/builds/@@'`
+  export CI_BRANCH=${CI_BUILD_REF_NAME}
+  export CI_OS_NAME=${CI_OS_NAME:-linux}
+}
+
+_travis_setup_ci_vars()
+{
+  export CI_REQUIRE_SUDO=${CI_REQUIRE_SUDO:-true}
+  export CI_PULL_REQUEST=${TRAVIS_PULL_REQUEST}
+  export CI_REPO_SLUG=${TRAVIS_REPO_SLUG}
+  export CI_BRANCH=${TRAVIS_BRANCH}
+  export CI_OS_NAME=${TRAVIS_OS_NAME:-linux}
+}
+
+# _setup_ci_vars
+# --------------
+#
+# Setup CI_* variables based on the CI type
+_setup_ci_vars()
+{
+  # Check which CI tool we are using, default to travis
+  export CI_TOOL=${CI_TOOL:-travis}
+
+  if [ $CI_TOOL = travis ]; then
+    _travis_setup_ci_vars
+  else
+    _gitlab_setup_ci_vars
+  fi
+}
+
+# _setup_sudo_cmd
+# ---------------
+#
+# Setup SUDO_CMD based on CI configuration
+_setup_sudo_cmd()
+{
+  if [ ${CI_REQUIRE_SUDO} = false ]; then
+    export SUDO_CMD=''
+  else
+    export SUDO_CMD='sudo'
+  fi
+}
+
+# _setup_ros
+# ----------
+#
+# Setup ROS environment if present on the system
+_setup_ros()
+{
+  if [ -f /opt/ros/${ROS_DISTRO}/setup.sh ]; then
+    . /opt/ros/${ROS_DISTRO}/setup.sh
+  fi
+}
+
+# _setup_env_vars
+# ---------------
+#
+# Setup environment variables
+_setup_env_vars()
+{
+  export LD_LIBRARY_PATH="$install_dir/lib:$LD_LIBRARY_PATH"
+  export LTDL_LIBRARY_PATH="$install_dir/lib:$LTDL_LIBRARY_PATH"
+  export PKG_CONFIG_PATH="$install_dir/lib/pkgconfig:$PKG_CONFIG_PATH"
+  if type "python" > /dev/null; then
+    pythonsite_dir=`python -c "import sys, os; print(os.sep.join(['lib', 'python' + sys.version[:3], 'site-packages']))"`
+    export PYTHONPATH="$install_dir/$pythonsite_dir:$PYTHONPATH"
+  fi
+
+}
+
+# _setup_linux_env
+# ----------------
+#
+# Environment setup specific to linux
+_setup_linux_env()
+{
+  export LD_LIBRARY_PATH="$install_dir/lib/`dpkg-architecture -qDEB_BUILD_MULTIARCH`:$LD_LIBRARY_PATH"
+  export LTDL_LIBRARY_PATH="$install_dir/lib/`dpkg-architecture -qDEB_BUILD_MULTIARCH`:$LTDL_LIBRARY_PATH"
+  export PKG_CONFIG_PATH="$install_dir/lib/`dpkg-architecture -qDEB_BUILD_MULTIARCH`/pkgconfig:$PKG_CONFIG_PATH"
+}
+
+# _setup_osx_env
+# ----------------
+#
+# Environment setup specific to OSX
+_setup_osx_env()
+{
+  # Since default gcc on osx is just a front-end for LLVM...
+  if [[ ${CC} = gcc ]]; then
+    export CXX=g++-4.8
+    export CC=gcc-4.8
+  fi
+}
+
+# setup_ci_env
+# ------------
+#
+# Setup the CI and environment variables
+setup_ci_env()
+{
+  _setup_ci_vars
+  _setup_sudo_cmd
+  _setup_ros
+  _setup_env_vars
+  if [[ ${CI_OS_NAME} = linux ]]; then
+    _setup_linux_env
+  fi
+  if [[ ${CI_OS_NAME} = osx ]]; then
+    _setup_osx_env
+  fi
+}
+
+# Directories.
+root_dir=`pwd`
+build_dir="/tmp/_ci/build"
+install_dir="/tmp/_ci/install"
+
+echo "root_dir: " $root_dir
+echo "build_dir: " $build_dir
+echo "install_dir: " $install_dir
+
+# Shortcuts.
+git_clone="git clone --quiet --recursive"
+
+# Setup all variables needed by the CI scripts
+setup_ci_env
+
+# Make cmake verbose.
+export CMAKE_VERBOSE_MAKEFILE=1
+export CTEST_OUTPUT_ON_FAILURE=1
+
+# Create layout.
+mkdir -p "$build_dir"
+mkdir -p "$install_dir"
