63ad03a9e5af21c13932b9a02ba904205cd46680
Zbitou Zakaria <zbitou.zakaria@gmail.com>
2017-05-02 14:07:32 +0900

    [FIX] benchmark build process

--- dependencies/benchmark.sh
+++ dependencies/benchmark.sh
@@ -2,12 +2,14 @@
 #
 # Setup Google Micro Benchmark Framework
 #
+. `dirname $0`/../common.sh
 
 cd "$build_dir"
 git clone "https://github.com/google/benchmark.git" "$build_dir/benchmark"
 cd "$build_dir/benchmark"
-mkdir "$build_dir/build"
-cd "$build_dir/build"
+mkdir -p "$build_dir/benchmark/_build"
+cd "$build_dir/benchmark/_build"
+
 cmake .. -DCMAKE_INSTALL_PREFIX:STRING="$install_dir"
 make
 make install
